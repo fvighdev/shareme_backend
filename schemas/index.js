63bffb718c7f2bf8a1ac4460cbc67140c0cb8b05
Ferenc Vigh <fvigh.dev@gmail.com>
2024-01-18 12:39:40 +0100

    starter setup for with data schemas for sanity backend is created

---
+++ schemas/index.js
@@ -0,0 +1,7 @@
+import userSchema from './userSchema'
+import pinSchema from './pinSchema'
+import commentSchema from './commentSchema'
+import postedBySchema from './postedBySchema'
+import saveSchema from './saveSchema'
+
+export const schemaTypes = [userSchema, pinSchema, commentSchema, postedBySchema, saveSchema]
